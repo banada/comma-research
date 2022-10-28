5c2a07122d680dd900f8f0eee1890567bef2e4c2
Nathaniel Chen <nathanielchen1@gmail.com>
2022-10-27 19:06:05 -0700

    Split out a test set

--- split-dataset.py
+++ split-dataset.py
@@ -7,8 +7,10 @@ dataset_dir = os.environ.get('BODY_DATASET_DIR')
 try:
   os.mkdir(f'{dataset_dir}/camera/training')
   os.mkdir(f'{dataset_dir}/camera/validation')
+  os.mkdir(f'{dataset_dir}/camera/test')
   os.mkdir(f'{dataset_dir}/log/training')
   os.mkdir(f'{dataset_dir}/log/validation')
+  os.mkdir(f'{dataset_dir}/log/test')
 except:
   print('training/validation directories already exist')
 
@@ -19,11 +21,18 @@ print(f"{len(file_list)} files")
 
 random.shuffle(file_list)
 valid_ratio = 0.2
+test_ratio = 0.1
+
 
 for idx, file in enumerate(file_list):
-  if ((idx/len(file_list)) > (1-valid_ratio)):
+  # Training
+  if ((idx/len(file_list)) < (1-valid_ratio-test_ratio)):
+    shutil.move(f"{dataset_dir}/camera/{file}", f"{dataset_dir}/camera/training/{file}")
+    shutil.move(f"{dataset_dir}/log/{file}", f"{dataset_dir}/log/training/{file}")
+  # Validation
+  elif ((idx/len(file_list)) < (1-test_ratio)):
     shutil.move(f"{dataset_dir}/camera/{file}", f"{dataset_dir}/camera/validation/{file}")
     shutil.move(f"{dataset_dir}/log/{file}", f"{dataset_dir}/log/validation/{file}")
   else:
-    shutil.move(f"{dataset_dir}/camera/{file}", f"{dataset_dir}/camera/training/{file}")
-    shutil.move(f"{dataset_dir}/log/{file}", f"{dataset_dir}/log/training/{file}")
+    shutil.move(f"{dataset_dir}/camera/{file}", f"{dataset_dir}/camera/test/{file}")
+    shutil.move(f"{dataset_dir}/log/{file}", f"{dataset_dir}/log/test/{file}")
